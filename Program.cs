ec8f5e8455c2c14453c14da393435d03136832ad
sosaf <207451605+sosaf-ctrl@users.noreply.github.com>
2025-12-17 20:39:18 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using H3XStealer;
+using System;
+using System.Windows.Forms;
+
+#nullable disable
+namespace H3XTools;
+
+internal static class Program
+{
+  [STAThread]
+  private static void Main()
+  {
+    try
+    {
+      Application.EnableVisualStyles();
+      Application.SetHighDpiMode(HighDpiMode.SystemAware);
+      ApplicationConfiguration.Initialize();
+      
+      SplashScreen splash = new SplashScreen();
+      splash.ShowDialog();
+      
+      Application.Run((Form) new MainForm());
+    }
+    catch (Exception ex)
+    {
+      int num = (int) MessageBox.Show($"> An error occurred while starting the application: {ex.Message} <", "H3X Startup - Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+    }
+  }
+}
